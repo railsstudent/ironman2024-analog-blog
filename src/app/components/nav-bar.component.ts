840a71b2fbbf14d21962901b26578b62f0d8798e
Connie Leung <cleung334@gmail.com>
2024-09-20 09:33:05 +0800

    feat: create NavFooterComponent

--- src/app/components/nav-bar.component.ts
+++ src/app/components/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -24,6 +24,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
       text-decoration: underline;
     }
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavBarComponent {
   links = [
