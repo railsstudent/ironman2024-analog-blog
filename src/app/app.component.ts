b0e55c807ad659fbc949be88c015995e6668568b
Connie Leung <cleung334@gmail.com>
2024-09-20 08:32:56 +0800

    feat: create NavFooterComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar.component';
+import { NavFooterComponent } from './components/nav-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, NavFooterComponent],
   template: `
     <blog-nav-bar class="navbar" />
-    <router-outlet></router-outlet>
+    <div class="main">
+      <router-outlet></router-outlet>
+    </div>
+    <blog-nav-footer class="nav-footer" />
   `,
   styles: `
       :host {
@@ -18,7 +22,7 @@ import { NavBarComponent } from './components/nav-bar.component';
         text-align: center;
         
         display: grid;
-        grid-template-rows: 60px 1fr 250px;
+        grid-template-rows: 60px 1fr 200px;
         grid-template-columns: 1fr;
         grid-template-areas: "header"
                              "body"
@@ -27,6 +31,14 @@ import { NavBarComponent } from './components/nav-bar.component';
 
       .navbar {
         grid-area: header;
+      }
+
+      .main, router-outlet + *  {
+        grid-area: body;
+      }
+
+      .nav-footer {
+        grid-area: footer;
       }
     `,
 })
---
+++ src/app/components/nav-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-nav-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-footer.component.html',
+  styleUrl: './nav-footer.component.css'
+})
+export class NavFooterComponent {
+
+}
