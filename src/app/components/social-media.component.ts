99d2459cd444bc10546e03e0e4a9bcc66920b199
Connie Leung <cleung334@gmail.com>
2024-09-21 10:34:30 +0800

    refactor:  create icon components

--- src/app/components/social-media.component.ts
+++ src/app/components/social-media.component.ts
@@ -10,14 +10,9 @@ import { SocialMedia } from '../types/social-media.type';
     <a [href]="info.href" target="_blank" [ariaDescription]="ariaDescription">
       <span class="mr-2">{{ info.text }}</span>  
       <ng-content />
-      <img [src]="assetFullPath()" [ariaDescription]="ariaDescription" />
     </a>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SocialMediaComponent {  
   information = input.required<SocialMedia>();
-  assetFullPath = computed(() => {
-    const filename = this.information().filename || 'github.svg';
-    return `/images/${filename}`;
-  })
 }
