a350a6ab5366bf58313fa2790fe7f6a2bb05fe20
Connie Leung <cleung334@gmail.com>
2024-09-26 09:16:05 +0800

    feat: add card component

---
+++ src/app/components/card.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-card',
+  standalone: true,
+  imports: [],
+  template: ``,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent {
+
+}
