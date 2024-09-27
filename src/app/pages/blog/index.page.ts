e7518eece3e92e8c95388faa46706a6185c893e0
Connie Leung <cleung334@gmail.com>
2024-09-27 16:05:11 +0800

    feat: add flex layout

--- src/app/pages/blog/index.page.ts
+++ src/app/pages/blog/index.page.ts
@@ -14,21 +14,25 @@ export const routeMeta: RouteMeta = {
   standalone: true,
   imports: [RouterLink, CardComponent],
   template: `
-    <h1>Blog Archive</h1>
-    @for (post of posts; track post.attributes.slug) {
-      @let attributes = post.attributes;
-      <blog-card>
-        <h2 class="m-0 pl-5 underline text-2xl mb-4">{{ attributes.title }}</h2>
-        <p class="text-left m-0 mb-4">{{ attributes.description }}</p>
-        <div footer class="flex justify-between px-5">
-          <p><span class="text-[#646cff]">Date Published:</span> {{ attributes.datePublished }}</p>
-          <a class="block" [routerLink]="['/blog/', attributes.slug]">Read</a>
-        </div>
-      </blog-card>
-    }
+    <h1 class="mb-6">Blog Archive</h1>
+    <div class="flex flex-wrap justify-around">
+      {{ posts.length }}
+      @for (post of posts; track post.attributes.slug) {
+        @let attributes = post.attributes;
+        <blog-card class="grow shrink basis-1/3 mb-8">
+          <h2 class="m-0 pl-5 underline text-2xl mb-4">{{ attributes.title }}</h2>
+          <p class="text-left m-0 mb-4">{{ attributes.description }}</p>
+          <div footer class="flex justify-between px-5">
+            <p><span class="text-[#646cff]">Date Published:</span> {{ attributes.datePublished }}</p>
+            <a class="block" [routerLink]="['/blog/', attributes.slug]">Read</a>
+          </div>
+        </blog-card>
+      }
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class BlogComponent {
-  readonly posts = injectContentFiles<PostAttributes>();
+  readonly posts = injectContentFiles<PostAttributes>((contentFile) =>
+  contentFile.filename.includes('/src/content'));
 }
---
+++ src/content/example-post-7.md
@@ -0,0 +1,15 @@
+---
+title: My First Post 7
+slug: 2022-12-25-my-first-post-7
+description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fringilla semper facilisis. Ut mattis et eros id sollicitudin. Aenean ut pellentesque odio. Aliquam quam odio, viverra elementum ultricies a, pharetra quis dui. Maecenas auctor orci venenatis velit aliquam tempus. Sed odio ante, maximus sit amet lacus at, fermentum feugiat nisl. Mauris dictum tincidunt iaculis. Fusce at ante eros.
+datePublished: '2022-12-25'
+coverImage: https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80
+---
+
+# Example Blog Post
+
+This is an example blog post written in markdown!
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fringilla semper facilisis. Ut mattis et eros id sollicitudin. Aenean ut pellentesque odio. Aliquam quam odio, viverra elementum ultricies a, pharetra quis dui. Maecenas auctor orci venenatis velit aliquam tempus. Sed odio ante, maximus sit amet lacus at, fermentum feugiat nisl. Mauris dictum tincidunt iaculis. Fusce at ante eros.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
