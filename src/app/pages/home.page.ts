efe45f3e24f01a4bd2517446b7de443836a19984
Connie Leung <cleung334@gmail.com>
2024-09-28 16:02:25 +0800

    refactor: dynamically create blog card in the home page

--- src/app/pages/home.page.ts
+++ src/app/pages/home.page.ts
@@ -15,37 +15,54 @@ export const routeMeta: RouteMeta = {
     <section class="introduction mb-4">
         <p>Hello. My name is Connie and I am an Angular GDE from Hong Kong.</p>
         <p>I am a passionate software engineer and content creator who is constantly learning about the latest web technology to level up my skills.</p>
-        <p>When I am not working, I love to exercise, learn foreign languages, and create content    to show the new features and best practices of the <span class="framework underline italic">Angular</span> framework.</p>
+        <p>When I am not working, I love to exercise, learn foreign languages, and create content to show the new features and best practices of the <span class="framework underline italic">Angular</span> framework.</p>
     </section>
     <section class="flex justify-evenly">
-        <blog-card>
-            <p header class="text-xl underline">Current Technical Stack<p>
-            <ul class="text-left list-disc p-5">
-                <li>Angular</li>
-                <li>Vue (Composition API)</li>
-                <li>NestJS</li>
-                <li>JavaScript</li>
-                <li>HTMX</li>
-                <li>PostgreSQL</li>
-            </ul>
-        </blog-card>
-        <blog-card>
-            <p header class="text-xl underline">Areas of Interest<p>
-            <ul class="text-left list-disc p-5">
-                <li>Angular Architecture</li>
-                <li>REST API</li>
-                <li>Generative AI</li>
-            </ul>
-        </blog-card>
-        <blog-card>
-            <p header class="text-xl underline">Hobbies<p>
-            <ul class="text-left list-disc p-5">
-                <li>Learn Foreign Language</li>
-                <li>Learn Tech</li>
-                <li>Exercise</li>
-            </ul>
-        </blog-card>
+        @for (content of cardContents; track $index) {
+            <blog-card>
+                <p header class="text-xl underline">{{ content.title }}<p>
+                <ul class="text-left list-disc p-5">
+                    @for (item of content.items; track item) {
+                        <li>{{ item }}</li>
+                    }
+                </ul>
+            </blog-card>
+        }
     </section>
   `,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+    cardContents = [
+        {
+            title: 'Current Technical Stack',
+            items: [
+                "Angular",
+                "Vue (Composition API)",
+                "NestJS",
+                "JavaScript",
+                "HTMX",
+                "PostgreSQL"
+            ]
+        },
+        {
+            title: "Areas of Interest",
+            items: [
+                "Angular Architecture",
+                "REST API",
+                "GraphQL",
+                "Generative AI",
+            ]
+        },
+        {
+            title: "Hobbies",
+            items: [
+                "Learn Foreign Languages",
+                "Learn Tech",
+                "Exercise",
+                "Blogging",
+                "Watch Wrestling and make podcasts",
+                "Travelling"
+            ]
+        }
+    ]
+}
